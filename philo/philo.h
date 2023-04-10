48f06e06629ccc263b9dda82636780b783ff1117
flytoox <omarprince823@gmail.com>
2023-04-10 19:54:40 +0000

    Step one

--- philo/philo.h
+++ philo/philo.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philo.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: obelaizi <obelaizi@student.42.fr>          +#+  +:+       +#+        */
+/*   By: obelaizi <obelaizi@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/30 03:39:25 by obelaizi          #+#    #+#             */
-/*   Updated: 2023/03/30 03:40:51 by obelaizi         ###   ########.fr       */
+/*   Updated: 2023/04/07 02:52:06 by obelaizi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,18 @@
 # include <fcntl.h>
 # include <sys/wait.h>
 # include <pthread.h>
+# include <sys/time.h>
 
+typedef struct phl
+{
+	
+}	t_phl;
+
+typedef struct gnrl
+{
+	long long	*args;
+}	t_gnrl;
+
+long long	ft_atoi(const char *str);
 
 #endif
